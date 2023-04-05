c6041421ea4a7b103c07ba55ac72faee452998e4
Michaelai0 <alphasni@gmail.com>
2023-04-05 05:39:56 -0500

    changes to application_controllers and likes

--- app/controllers/api/v1/likes_controller.rb
+++ app/controllers/api/v1/likes_controller.rb
@@ -1,24 +1,48 @@
 module Api
   module V1
     class LikesController < Api::V1::ApplicationController
-      before_action :authenticate_user!
       before_action :set_tweet
 
       def create
-        like = Like.new(user: current_user, tweet: @tweet)
+        if @tweet.user == current_user
+          # User is trying to like their own tweet
+          render_error(
+            errors: "You cannot like your own tweet",
+            status: :unprocessable_entity
+          )
+          return
+        end
+
+        like = Like.new(user: @current_user, tweet: @tweet)
         if like.save
-          render_success(payload: { like: LikeBlueprint.render_as_hash(like) }, status: :created)
+          render_success(
+            payload: {
+              like: LikeBlueprint.render_as_hash(like)
+            },
+            status: :created
+          )
         else
-          render_error(errors: like.errors.full_messages, status: :unprocessable_entity)
+          render_error(
+            errors: like.errors.full_messages,
+            status: :unprocessable_entity
+          )
         end
       end
 
       def destroy
-        like = Like.find_by(user: current_user, tweet: @tweet)
+        like = Like.find_by(user: @current_user, tweet: @tweet)
         if like.destroy
-          render_success(payload: { message: 'Like removed successfully' }, status: :ok)
+          render_success(
+            payload: {
+              message: "Like removed successfully"
+            },
+            status: :ok
+          )
         else
-          render_error(errors: like.errors.full_messages, status: :unprocessable_entity)
+          render_error(
+            errors: like.errors.full_messages,
+            status: :unprocessable_entity
+          )
         end
       end
 
